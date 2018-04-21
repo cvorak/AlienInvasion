fa08372141b700968a8a05657e472a0b389c7ca1
Nikola Čvorović <cvorak@gmail.com>
2018-04-21 20:01:37 +0200

    Add left and right movement to ship, add ship_speed_factor to settings.

--- AlienInvasion/alien_invasion.py
+++ AlienInvasion/alien_invasion.py
@@ -13,14 +13,15 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
 
     # Make a ship.
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
 
     # Set the background color.
     bg_color = (230, 230, 230)
 
     # Start the main loop for the game.
     while True:
-        gf.check_events()
+        gf.check_events(ship)
+        ship.update()
         gf.update_screen(ai_settings, screen, ship)
 
 
